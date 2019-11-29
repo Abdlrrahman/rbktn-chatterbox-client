25f811835203ff93a127db5d3db5625fc410abad
Abdlrrahman <56532938+Abdlrrahman@users.noreply.github.com>
2019-11-29 20:16:14 +0100

    1

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,11 +1,25 @@
 var MessagesView = {
 
   $chats: $('#chats'),
+  $message: $('#message'),
 
   initialize: function() {
+  	//MessagesView.$chats.on('submit', MessagesView.render)
   },
 
-  render: function() {
+  renderMessage: function() {
+    console.log(message)
+      this.$chats.append(MessageView.render({username: message.username, val: message.text})); 	
   }
 
 };
+
+/*
+var message = {
+  username: 'Mel Brooks',
+  text: 'Never underestimate the power of the Schwartz!',
+  roomname: 'lobby'
+};
+
+*/
+// FormView.$form.on('submit', FormView.handleSubmit);
--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,4 +1,11 @@
 var Rooms = {
-
+	render: _.template(`
+      
+      <div class="room">
+        <div class="roomname"></div>
+        <div></div>
+      </div>
+      
+    `)
 
 };
